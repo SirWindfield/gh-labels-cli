4daec02e06db29e77ce3214ef32a82577e25eadd
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-20 03:13:19 +0100

    refactor: reorganize extension module

--- src/extension/mod.rs
+++ src/extension/mod.rs
@@ -1,8 +1,2 @@
-mod label_already_exists;
-pub use label_already_exists::LabelAlreadyExistsExt;
-
-mod repo_not_found;
-pub use repo_not_found::RepoNotFoundExt;
-
-mod unauthorized;
-pub use unauthorized::*;
+mod hubcaps_error;
+pub use hubcaps_error::*;
